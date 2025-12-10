ac1de6b31c0716410d15bc7b6eb3925872eabe83
Avneesh Patel <avneeshpatel20842@gmail.com>
2025-12-10 19:24:09 +0530

    Final

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://job-portal-web-app-3.onrender.com"],
   credentials: true,
 };
 app.use(cors(corsOption));
